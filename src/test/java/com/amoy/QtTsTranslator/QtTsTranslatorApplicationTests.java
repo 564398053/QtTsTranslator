2080e966fcaa21a1776391901a30a0cb9049aee4
lijian <564398053@qq.com>
2021-10-08 23:54:44 +0800

    first commit

---
+++ src/test/java/com/amoy/QtTsTranslator/QtTsTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amoy.QtTsTranslator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QtTsTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
